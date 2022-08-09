ed4988d760a21f910f45d1e6bd17b8451e858bcf
Isaias Medina <isamed92@gmail.com>
2022-08-08 22:48:51 -0300

    finished section

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -11,11 +11,11 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () async {
-        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-        //     '#3d8bef', 'Cancelar', false, ScanMode.QR);
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3d8bef', 'Cancelar', false, ScanMode.QR);
         // print(barcodeScanRes);
         // const barcodeScanRes = 'https://onepiece.fandom.com/es/wiki/Nico_Robin';
-        const barcodeScanRes = 'geo:-26.812028,-65.198654';
+        // const barcodeScanRes = 'geo:-26.812028,-65.198654';
         if (barcodeScanRes == '-1') return;
         final scanListProvider =
             Provider.of<ScanListProvider>(context, listen: false);
